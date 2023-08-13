eef34ed7ae4fda925b339fbb44dee5d288d29e47
Noman Abid <nomanabid3557@gmail.com>
2023-08-13 16:19:40 +0500

    operators

--- index.ts
+++ index.ts
@@ -1,1 +1,32 @@
 //operators
+//arithmatic operators
+
+// + - * / % ++ --
+//x and y are operands and + is operator
+let x = 10;
+let y = 28;
+console.log(y - x);
+
+let result = x * y;
+// console.log(result);
+let divide = y / x;
+let reminder = y % x;
+
+console.log(divide);
+console.log(reminder);
+
+//increment and decrement operators
+//pre and post increment and decrement
+let z = ++y;
+
+let a =  --x
+console.log(a);
+
+//post increment and decrement
+let c = 12
+let b = c++;
+let d = c--;
+
+console.log(b);
+console.log(c);
+console.log(d);
